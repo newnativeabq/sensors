a4aaf9f776ae095d08926741e5337e7eee5b2cba
newnativeabq <vincent.a.brandon@gmail.com>
2020-12-28 17:17:10 +0000

    ENH: added start_stop test.  Works, but with caveat - rogue processes and blocking issues.  Need redis req

--- tests/test_start_stop.py
+++ tests/test_start_stop.py
@@ -11,6 +11,12 @@ def tsensor():
         device = Timer,
         reporter = Printer,
         freq = 0.25,
-        threaded = False
+        threaded = True
     )
     return s
+    
+
+
+def test_start_stop_sensor(tsensor):
+    tsensor.start()  # Will spawn rogue process if no redis connection... TODO?
+    tsensor.stop()  # this won't work with unthreaded start (blocks)
